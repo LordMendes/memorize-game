fba185148aa44527a79e7b5a0d3ea6d32c7bc062
Lucas <lucasmendes.lx@gmail.com>
2021-05-06 19:35:37 -0300

    Initial Commit

---
+++ Memorizze/MemorizzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizzeApp.swift
+//  Memorizze
+//
+//  Created by Lucas Mendes on 06/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
